039082fa0d890af033874214b3badf8dc6c62c8f
Aaron Rama <aaronrama@gmail.com>
2017-02-17 18:09:49 +1300

    Change ColourUtility to ensure valid rgb values

--- src/main/java/com/aaronrama/codeposter/ColourUtility.java
+++ src/main/java/com/aaronrama/codeposter/ColourUtility.java
@@ -9,13 +9,21 @@ import java.awt.Color;
 public class ColourUtility {
 
     public static String rgbToHex(int r, int g, int b) {
-        return  "#" + Integer.toString(r, 16) +
-                Integer.toString(g, 16) +
-                Integer.toString(b, 16);
+        return String.format("#%02x%02x%02x", normalize(r), normalize(g), normalize(b));
     }
 
+    // Have disabled alpha for a bit as it was causing issues.
     public static String rgbaToHex(int r, int g, int b, int a) {
-        return String.format("#%02x%02x%02x",r, g, b);
+        return String.format("#%02x%02x%02x", normalize(r), normalize(g), normalize(b));
+    }
 
+    private static int normalize(int val) {
+        if (val > 255) {
+            return 255;
+        } else if (val < 0) {
+            return 0;
+        } else {
+            return val;
+        }
     }
 }
